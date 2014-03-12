f358e22334b9592f53ea9a09152a943e01a7dab7
Paulino Rolon <paurolon@creadores.com.py>
2014-03-12 01:43:17 -0700

    Update html5audio.js

--- www/scripts/html5audio.js
+++ www/scripts/html5audio.js
@@ -1,5 +1,4 @@
 
-
 var progressTimer;
 
 var playButton;
@@ -22,7 +21,7 @@ function pad2(number) {
 	return (number < 10 ? '0' : '') + number
 }
 
-var myaudioURL = 'http://stream.4zzzfm.org.au:789/;';
+var myaudioURL = 'http://198.23.165.148:8888/;';
 var myaudio = new Audio(myaudioURL);
 var isPlaying = false;
 var readyStateInterval = null;
